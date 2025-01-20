d3492d376adb0a23e6ec105835f83d9eebb0598e
Dave <69651599+D4ve-R@users.noreply.github.com>
2025-01-20 12:18:45 +0100

    add dto response to list request

--- src/Requests/Instance/List.php
+++ src/Requests/Instance/List.php
@@ -1,9 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace D4veR\LambdaLabs\Requests\Instance;
 
+use D4veR\LambdaLabs\Data\Instance;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Http\Response;
 
 class ListInstances extends Request
 {
@@ -12,5 +16,17 @@ class ListInstances extends Request
     public function resolveEndpoint(): string
     {
         return '/instances';
+    }
+
+    public function createDtoFromResponse(Response $response): array
+    {
+        $data = $response->json();
+        $instances = [];
+
+        foreach ($data['data'] as $instance) {
+            $instances[] = Instance::fromArray($instance);
+        }
+
+        return $instances;
     }
 }
